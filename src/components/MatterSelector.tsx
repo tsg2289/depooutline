0857520a1cff84ef63899e2b4407932fddb91dbc
tsg2289 <thomas.st.germain22@gmail.com>
2025-09-28 12:29:00 -0700

    Remove selection indicator from unselected matters view
    - Removed the checkmark indicator that was causing TypeScript error
    - In 'show all matters' mode, no matter should show as selected
    - Fixes the remaining TypeScript compilation error on line 344
    - Clean separation between selection mode and selected matter display

--- src/components/MatterSelector.tsx
+++ src/components/MatterSelector.tsx
@@ -341,15 +341,6 @@ export function MatterSelector({ selectedMatter, onMatterSelect }: MatterSelecto
                     </button>
                   </div>
                 </div>
-                {selectedMatter?.id === matter.id && (
-                  <div className="absolute -top-1 -right-1">
-                    <div className="w-6 h-6 bg-indigo-500 rounded-full flex items-center justify-center">
-                      <svg className="w-3 h-3 text-white" fill="currentColor" viewBox="0 0 20 20">
-                        <path fillRule="evenodd" d="M16.707 5.293a1 1 0 010 1.414l-8 8a1 1 0 01-1.414 0l-4-4a1 1 0 011.414-1.414L8 12.586l7.293-7.293a1 1 0 011.414 0z" clipRule="evenodd" />
-                      </svg>
-                    </div>
-                  </div>
-                )}
               </div>
             </div>
           ))
